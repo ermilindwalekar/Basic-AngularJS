0cb85defa6afead06f23fb654260d0a8fd044e1c
ermilindwalekar <ermilindwalekar@gmail.com>
2016-08-30 16:05:45 +0530

    Add files via upload

--- AngularTask1/WebContent/js/product.js
+++ AngularTask1/WebContent/js/product.js
@@ -1,7 +1,8 @@
 var myModule = angular.module('productApplication', ['ngRoute']);
-
 function productFactory(){
 	var factoryObj={};
+
+	var index=-1;
 	factoryObj.products=[{id:1,name:"Chair",price:2000,quantity:15},{id:2,name:"Table",price:2500,quantity:10}];
 	var id=3;
 	factoryObj.add_product = function (name,quantity,price) {
@@ -11,51 +12,47 @@ function productFactory(){
 	factoryObj.delete_product = function (index) {
 		factoryObj.products.splice(index, 1);
     }
-	factoryObj.edit_product = function (index,$scope) {
-		console.log(factoryObj.products[index].id);
-		console.log(factoryObj.products[index].name);
-		console.log(factoryObj.products[index].quantity);
-		console.log(factoryObj.products[index].price);
-	}
-	factoryObj.update_product = function (id,name,quantity,price,index) {
-		
-		factoryObj.products[index]={id:id,name:name,quantity:quantity,price:price};
-		console.log("-------------UPDATED-------------");
-		console.log(factoryObj.products[index].id);
-		console.log(factoryObj.products[index].name);
-		console.log(factoryObj.products[index].quantity);
-		console.log(factoryObj.products[index].price);
-		
+	factoryObj.update_product = function (id,name,quantity,price) {
+		var index=0;
+		for(var j=0;j< factoryObj.products.length ;j++)
+		{
+			if(factoryObj.products[j].id == id)
+			{
+				index=j;
+				factoryObj.products[index]={name:name,quantity:quantity,price:price};
+				console.log("-------------UPDATED-------------");
+				console.log(factoryObj.products[index].id);
+				console.log(factoryObj.products[index].name);
+				console.log(factoryObj.products[index].quantity);
+				console.log(factoryObj.products[index].price);
+				break;
+			}
+		}
+			
     }
 	return factoryObj;
 }
 myModule.factory("ProductFactory",productFactory);
 
-function productController($scope,ProductFactory) {
+function productController($scope,ProductFactory,$location) {
 	$scope.products=ProductFactory.products;
-	var index=-1;
+
 	$scope.add= function() {
 		
 		ProductFactory.add_product($scope.name,$scope.quantity,$scope.price);
 	}
 	$scope.del= function(product) {
-		index = $scope.products.indexOf(product);
-		console.log(index);
+		var index = $scope.products.indexOf(product);
 		ProductFactory.delete_product(index);
 	}
 	$scope.edit= function(product) {
-		index = $scope.products.indexOf(product);
-		ProductFactory.edit_product(index);
+		var index = $scope.products.indexOf(product);
+		$location.path('/edit_product/' + product.id+"/" +product.name+"/"+product.quantity+"/"+product.price);
 		
 	}
 	$scope.update= function() {
-		console.log("---------Grasp Value-------");
-		console.log($scope.id);
-		console.log($scope.name);
-		console.log($scope.quantity);
-		console.log($scope.price);
 		
-		ProductFactory.update_product($scope.id,$scope.name,$scope.quantity,$scope.price,index);
+		ProductFactory.update_product($scope.id,$scope.name,$scope.quantity,$scope.price);
 	}
 	
 }
@@ -67,8 +64,14 @@ myModule.config(function($routeProvider){
 			controller: 'ProductController',
 			templateUrl: 'show_product.html'
 		})
-		.when('/editProducts', {
-			controller: 'ProductController',
+		.when("/edit_product/:id/:name/:quantity/:price", {
+			controller: function($scope, ProductFactory, $routeParams) {
+				$scope.id = $routeParams.id;
+				$scope.name = $routeParams.name;
+				$scope.quantity = $routeParams.quantity;
+				$scope.price = $routeParams.price;
+				
+			},
 			templateUrl: 'edit_product.html'
 		})
 		.otherwise({redirectTo: '/'})
